fa967538ad0f6b3e32f602702a7f453a28a4216c
JoaoGMB <muller.bordin@gmail.com>
2026-03-23 22:01:29 -0300

    Add files via upload

---
+++ entity/Categoria.java
@@ -0,0 +1,30 @@
+package com.example.categorias.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Categoria")
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_categoria")
+    private String nome;
+
+    @Column(name = "descricao_categoria")
+    private String descricao;
+
+    public Categoria() {}
+
+    public Long getId() { return id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+}
